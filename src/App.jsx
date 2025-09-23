d0f3efcdbfbc7edb95fda41690df666f96d8c889
AbdurRob <abdurrobr34l@gmail.com>
2025-09-23 21:37:40 +0600

    project setup and include Tailwind and daisiUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Start Your Coding Here</h1>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;
